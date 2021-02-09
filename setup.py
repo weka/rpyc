2f50485e3c5cb73296c9eee7c21deb59dcb73652
Anton Bykov <tigrawap@gmail.com>
2021-02-09 02:36:29 +0200

    Freeze plumbum version to 1.6.9

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name="rpyc",
       ],
       tests_require=['nose'],
       test_suite='nose.collector',
-      install_requires=["plumbum"],
+      install_requires=["plumbum==1.6.9"],
       #    entry_points = dict(
       #        console_scripts = [
       #            "rpyc_classic = rpyc.scripts.rpyc_classic:main",
